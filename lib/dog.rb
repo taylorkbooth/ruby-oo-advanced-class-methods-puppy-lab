c62f9e7247bac84fbeb2a1e53fcc94f66a4011f5
Taylor Booth <taylorkbooth@gmail.com>
2020-11-17 22:22:38 -0500

    commit tree e8d26832c16c69596a2cf70ca0484764eca43a40

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
-# Add your code here
+require 'pry'
+
+class Dog
+
+    attr_accessor :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+        puts @@all.collect { |dog| dog.name }
+    end
+
+    def save
+        @@all << self
+    end
+end
